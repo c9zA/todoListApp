8faec5250dfa5ee6db42127dd0ddbe17e07efec2
c9zA <177842201@qq.com>
2025-09-10 21:33:48 -0400

    Initial commit: Set up project with Vite, Tailwind CSS, and Prettier

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+//this file is for configure and using Vite
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+    plugins: [
+        tailwindcss(),
+    ],
+});
